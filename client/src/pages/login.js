71dcf0d9f915aeace1c36373de1a86ee97f679e9
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 18:02:52 -0500

    classes and localstorage in login js

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -4,3 +4,25 @@ import LogInPageComponent from "../components/LogInPage/logInPage";
 import OurModal from "../components/SignUpPage/modal";
 import axios from "axios"
 import { Link, withRouter } from "react-router-dom";
+
+class Login extends React.Component {
+    state = {
+        loggedIn: false,
+        username: "",
+        password: "",
+        visible: false
+    }
+
+    login = (credentials) => {
+        let that = this;
+        axios.post("/login", credentials)
+        .then((response) => {
+
+            localStorage.setItem("userID", response.data.id);
+            localStorage.setItem("username", response.data.username);
+            localStorage.setItem("userImage", response.data.image);
+
+            this.setState({loggedIn: true}, () => {
+                localStorage.setItem("loggedIn", true);
+                window.location.replace("/");
+            });
