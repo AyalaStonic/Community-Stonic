29c511fc8a0a91b19cd02f5c4ce38ba7ec4910a8
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 17:34:54 -0500

    handleInputChange

--- client/src/pages/createevent.js
+++ client/src/pages/createevent.js
@@ -16,3 +16,54 @@ class CreateEvent extends React.Component {
         time: "",
         description: "",
     }
+
+    createEvent = () => {
+
+        let formData = new FormData();
+        formData.append("name", this.state.eventName);
+        formData.append("address", localStorage.getItem("eventAddress"));
+        formData.append("date", this.state.date);
+        formData.append("time", this.state.time);
+        formData.append("description", this.state.description);
+        formData.append("organizer", this.state.username);
+        formData.append("image", this.state.image);
+
+        axios.post("/event", formData)
+            .then((response) => {
+                console.log(response);
+                localStorage.setItem("eventAddress", "")
+                window.location.replace("/");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    setImage = event => {
+        this.setState({image: event.target.files[0]})
+    }
+
+    handleInputChange = event => {
+
+        const name = event.target.name;
+        let value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.createEvent();
+    };
+
+    manageLogin = () => {
+        if (this.state.loggedIn === "true") {
+            localStorage.setItem("username", "");
+            localStorage.setItem("loggedIn", "false");
+            localStorage.setItem("userID", "");
+            localStorage.setItem("userImage", "");
+            this.setState({username: "", loggedIn: "false", userID: ""});
+        }
+    }
