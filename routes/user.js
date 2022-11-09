eeb60092409f02d1c0b88f06fb1180b96b2288ff
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 14:28:08 -0500

    posting r oute to create a new user

--- routes/user.js
+++ routes/user.js
@@ -9,3 +9,24 @@ router.get("/user", function (req, res) {
     db.Users.find({})
         .then((response) => res.json(response))
 });
+
+// post route to create a new user
+router.post("/user", parser.single("image"), (req, res) => {
+    console.log("api POST request received");
+    console.log(req.file);
+    const newUser = {};
+    let image = {};
+
+    if (req.file) {
+        image.url = req.file.url;
+        image.id = req.file.public_id;
+    } else {
+        image = req.body.image
+    }
+
+    newUser.username = req.body.username;
+    newUser.firstname = req.body.firstname;
+    newUser.lastname = req.body.lastname;
+    newUser.password = req.body.password;
+    newUser.email = req.body.email;
+    newUser.image = image;
