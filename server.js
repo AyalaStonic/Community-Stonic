967647db626d7f30f42912d07da40f4caf89715f
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 16:10:03 -0500

    Connect to the Mongo DB

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const session = require("express-session");
+const passport = require("./passport");
+const MongoStore = require("connect-mongo")(session);
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3001;
+
