eb1f5d5c9f88f6958d80e79d7eb82db45b361beb
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-08 20:46:25 -0500

    using this to store images

---
+++ cloudinary/cloudinary.js
@@ -0,0 +1,6 @@
+//  using multer and cloudinary to store user uploaded images
+const cloudinary = require("cloudinary");
+const multer = require("multer");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const dotenv = require('dotenv');
+dotenv.config()
