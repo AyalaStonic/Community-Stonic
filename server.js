ae7fb13c5bf7a468fb8ff00d5fcfdfacb69e36f0
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 16:11:32 -0500

    middleware

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const MongoStore = require("connect-mongo")(session);
 const morgan = require("morgan");
 const PORT = process.env.PORT || 3001;
 
+// Connect to the Mongo DB
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/volunteam";
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true },
+  console.log("Connected to MongoDB!")
+);
+
+// Define middleware here
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
