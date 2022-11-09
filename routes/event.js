65250a50a22bc4e2356c36ce3a5722ae79652f0d
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 13:51:07 -0500

    Router get / it defaults end to be the end of the array

--- routes/event.js
+++ routes/event.js
@@ -4,3 +4,18 @@ const Moment = require("moment");
 var nodemailer = require('nodemailer');
 const parser = require("../cloudinary/cloudinary");
 const cloudinary = require("cloudinary");
+
+router.get("/event", function (req, res) {
+    db.Events.find({}).then((response) => {
+
+        console.log(response);
+        console.log("showing events");
+        if (response.length > 5) {
+            let lastFive = response.slice(Math.max(response.length - 7, 1));
+            res.json(lastFive);
+        }
+        else {
+            res.json(response);
+        }
+    });
+});
