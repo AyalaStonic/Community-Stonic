ad5a3752d14f19fea34d24b09f6c5aa8f2e1dc3d
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-09 18:02:06 -0500

    importing log in components

---
+++ client/src/pages/login.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Nav from "../components/mainpage/nav"
+import LogInPageComponent from "../components/LogInPage/logInPage";
+import OurModal from "../components/SignUpPage/modal";
+import axios from "axios"
+import { Link, withRouter } from "react-router-dom";
