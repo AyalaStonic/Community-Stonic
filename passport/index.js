36ad42b7937bea4236541339688e6b1039c7c084
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-13 16:33:20 -0500

    adding to index js the user object attaches to req.user

--- passport/index.js
+++ passport/index.js
@@ -2,15 +2,7 @@ const passport = require("passport");
 const LocalStrategy = require('./localStrategy');
 const db = require('../model');
 
-// called on login, saves the id to session req.session.passport.user = {id:'..'}
-passport.serializeUser((user, done) => {
-    console.log('*** serializeUser called, user: ');
-    console.log(user); // the whole raw user object!
-    console.log('---------');
-    done(null, { _id: user._id });
-});
-
-// user object attaches to the request as req.user
+// user object attaches to req.user
 passport.deserializeUser((id, done) => {
     console.log('DeserializeUser called');
     db.Users.findOne(
@@ -24,6 +16,15 @@ passport.deserializeUser((id, done) => {
         }
     );
 });
+
+// called on login
+passport.serializeUser((user, done) => {
+    console.log('*** serializeUser called, user: ');
+    console.log(user); // the whole raw user object!
+    console.log('---------');
+    done(null, { _id: user._id });
+});
+
 
 //  Use Strategies 
 passport.use(LocalStrategy);
