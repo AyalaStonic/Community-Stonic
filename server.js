aa43683e192393c2e32cc0705eb658ab4b9924f8
AyalaStonic <ayaladuvan876@gmail.com>
2022-11-13 10:58:14 -0500

    Adding moongoose db and fixing a html view event page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 // Connect to the Mongo DB
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/volunteam";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/CommunityStonic";
 mongoose.connect(
   MONGODB_URI,
   { useNewUrlParser: true },
